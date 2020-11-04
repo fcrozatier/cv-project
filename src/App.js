b5a0c8c5396b7786f84b1ff4202be70dc67be249
fcrozatier <frederic.crozatier@protonmail.com>
2020-11-04 17:51:32 +0100

    Create TextInput component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import './App.css';
-import Section from './components/Section';
+import "./App.css";
+import Section from "./components/Section";
+import "./components/inputs/TextInput";
+import TextInput from "./components/inputs/TextInput";
 
 function App() {
   return (
     <div className="App">
       <header>CV App</header>
-      <Section fields={[
-                ["name", "text"], 
-                ["email", "email"], 
-                ["phone", "phone"]]}/>
-    </div>  
-  )
+      <Section
+        fields={[
+          ["name", "text"],
+          ["email", "email"],
+          ["phone", "phone"],
+        ]}
+      />
+      <TextInput label="Name" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import './Section.css';
-import './buttons.css';
+import React from "react";
+import "./Section.css";
+import "./buttons.css";
 
 class Section extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = this.initializeState(this.props.fields);
   }
 
-  initializeState(fields){
-    const entries = fields.map(item => [item[0], ""] );
+  initializeState(fields) {
+    const entries = fields.map((item) => [item[0], ""]);
     return Object.fromEntries(entries);
   }
 
-  displayForm = () => {
-    
-  }
+  displayForm = () => {};
 
-  render(){
+  render() {
     console.log(this.state);
     return (
       <div className="container">
-        <div className="card">
-          Hey there !
-        </div>
-        <button onClick={this.displayForm} className="btn btn-rounded btn-info">+</button>
+        <div className="card">Hey there !</div>
+        <button onClick={this.displayForm} className="btn btn-rounded btn-info">
+          +
+        </button>
       </div>
-    )
+    );
   }
 }
 
