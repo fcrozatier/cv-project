42a33cf7aaa9ba5d6456786df32cd55862790c28
fcrozatier <frederic.crozatier@protonmail.com>
2020-11-04 16:46:35 +0100

    Delete boilerplate

---
+++ src/components/Section.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Section.css';
+import './buttons.css';
+
+class Section extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = this.initializeState(this.props.fields);
+  }
+
+  initializeState(fields){
+    const entries = fields.map(item => [item[0], ""] );
+    return Object.fromEntries(entries);
+  }
+
+  displayForm = () => {
+    
+  }
+
+  render(){
+    console.log(this.state);
+    return (
+      <div className="container">
+        <div className="card">
+          Hey there !
+        </div>
+        <button onClick={this.displayForm} className="btn btn-rounded btn-info">+</button>
+      </div>
+    )
+  }
+}
+
+export default Section;
